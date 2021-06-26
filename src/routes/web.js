12666af88dd14518d4344c993dd73d73f6d0c41f
Nico Aco <nicoaco@gmail.com>
2021-06-26 18:36:04 -0300

    creacion archivo register.html - Agregado de ruta /register a routes/web.js

--- src/routes/web.js
+++ src/routes/web.js
@@ -6,5 +6,7 @@ const path=require("path");
 app.get ("/",(req,res)=>res.sendFile(path.resolve(__dirname,"../views","index.html")));
 app.get ("/loguin",(req,res)=>res.sendFile(path.resolve(__dirname,"../views","loguin.html")));
 app.get ("/shop",(req,res)=>res.sendFile(path.resolve(__dirname,"../views","shop.html")));
+app.get ("/register", (req,res) => res.sendFile(path.resolve(__dirname,"../views","register.html")));
+app.get ("/product", (req,res) => res.sendFile(path.resolve(__dirname,"../views","product.html")));
 
 module.exports = app;
