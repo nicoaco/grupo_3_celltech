b99cd67c57fc89d392e43d2856b711a40b8b49aa
martinbaldino <you@example.com>
2021-06-26 13:17:15 -0300

    creacion de register y  carrito

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ const path = require ('path')
 // app.get('/', (req,res) =>res.send("hola mundo") )
 app.listen(3000,() =>console.log("http://localhost:3000"))
 app.use(express.static(path.resolve(__dirname,"../public")))
+console.log(__dirname)
 
 app.get ("/",(req,res)=>res.sendFile(path.resolve(__dirname,"./views","index.html")))
+app.get ("/loguin",(req,res)=>res.sendFile(path.resolve(__dirname,"./views","loguin.html")))
+app.get ("/shop",(req,res)=>res.sendFile(path.resolve(__dirname,"./views","shop.html")))
+
 
 
 
