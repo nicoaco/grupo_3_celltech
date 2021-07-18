c11ed152e211024123b90dba58b67f1fd01f7eee
pauldimonte <paulo.di.monte@gmail.com>
2021-07-18 15:16:16 -0300

    vistas separadas en products  / users

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,10 +1,10 @@
 //const path=require("path");
 const mainController = {
     main:(req,res)=>res.render("index",{estilo:"index.css", titulo:"Celltech - Home"}),
-    login:(req,res)=>res.render("login",{estilo:"login.css", titulo:"Celltech - Login"}),
-    shop:(req,res)=>res.render("shop",{estilo:"shop.css", titulo:"Celltech - Shop"}),
-    register:(req,res) => res.render("register",{estilo:"register.css", titulo:"Celltech - Registracion"}),
-    product:(req,res) => res.render("product",{estilo:"product.css", titulo:"Celltech - Producto"})
+    login:(req,res)=>res.render("./users/login",{estilo:"login.css", titulo:"Celltech - Login"}),
+    shop:(req,res)=>res.render("./products/shop",{estilo:"shop.css", titulo:"Celltech - Shop"}),
+    register:(req,res) => res.render("./users/register",{estilo:"register.css", titulo:"Celltech - Registracion"}),
+    product:(req,res) => res.render("./products/product",{estilo:"product.css", titulo:"Celltech - Producto"})
 }
 
 module.exports= mainController
