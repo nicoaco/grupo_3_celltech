7817ed5b95fea44a0a9e545464a963248bda1c40
pauldimonte <paulo.di.monte@gmail.com>
2021-07-18 15:04:36 -0300

    styles y titulos aplicados desde el controlador

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,10 +1,10 @@
 //const path=require("path");
 const mainController = {
-    main:(req,res)=>res.render("index"),
-    login:(req,res)=>res.render("login"),
-    shop:(req,res)=>res.render("shop"),
-    register:(req,res) => res.render("register"),
-    product:(req,res) => res.render("product")
+    main:(req,res)=>res.render("index",{estilo:"index.css", titulo:"Celltech - Home"}),
+    login:(req,res)=>res.render("login",{estilo:"login.css", titulo:"Celltech - Login"}),
+    shop:(req,res)=>res.render("shop",{estilo:"shop.css", titulo:"Celltech - Shop"}),
+    register:(req,res) => res.render("register",{estilo:"register.css", titulo:"Celltech - Registracion"}),
+    product:(req,res) => res.render("product",{estilo:"product.css", titulo:"Celltech - Producto"})
 }
 
 module.exports= mainController
