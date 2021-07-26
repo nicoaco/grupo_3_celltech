5e3c6d8a4290e6c6016df70cf3f75df672d1bf17
Nico Aco <nicoaco@gmail.com>
2021-07-25 23:09:01 -0300

    cambios controller y views de productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,6 +1,122 @@
+let movil1 = {
+    id: 1,
+    nombre: "Galaxy Note 10+" ,
+    marca:"samsung",
+    os: "android",
+    core: 8,
+    memory: 2,
+    MpxCamera: 12,
+    descrip: "descripcion Smartphone",
+    precioLista: 75000,
+    descuento: 10,
+    precioFinal: 67500 
+};
+
+let movil2 = {
+    id: 2,
+    nombre: "Galaxy Note 10+" ,
+    marca: "samsung",
+    os: "android",
+    core: 8,
+    memory: 2,
+    MpxCamera: 12,
+    descrip: "descripcion Smartphone",
+    precioLista: 75000,
+    descuento: 10,
+    precioFinal: 67500 
+};
+
+let movil3 = {
+    id: 3,
+    nombre: "Galaxy Note 10+" ,
+    marca: "samsung",
+    os: "android",
+    core: 8,
+    memory: 2,
+    MpxCamera: 12,
+    descrip: "descripcion Smartphone",
+    precioLista: 75000,
+    descuento: 10,
+    precioFinal: 67500 
+};
+
+let movil4 = {
+    id: 4,
+    nombre: "Galaxy Note 10+" ,
+    marca: "samsung",
+    os: "android",
+    core: 8,
+    memory: 2,
+    MpxCamera: 12,
+    descrip: "descripcion Smartphone",
+    precioLista: 75000,
+    descuento: 10,
+    precioFinal: 67500 
+};
+
+let movil5 = {
+    id: 5,
+    nombre: "Apple XS" ,
+    marca: "apple",
+    os: "ios",
+    core: 8,
+    memory: 2,
+    MpxCamera: 12,
+    descrip: "descripcion Smartphone",
+    precioLista: 100000,
+    descuento: 10,
+    precioFinal: 90000 
+};
+
+let movil6 = {
+    id: 6,
+    nombre: "Apple XS" ,
+    marca: "apple",
+    os: "ios",
+    core: 8,
+    memory: 2,
+    MpxCamera: 12,
+    descrip: "descripcion Smartphone",
+    precioLista: 75000,
+    descuento: 10,
+    precioFinal: 67500 
+};
+
+let movil7 = {
+    id: 7,
+    nombre: "Apple XS" ,
+    marca: "apple",
+    os: "ios",
+    core: 8,
+    memory: 2,
+    MpxCamera: 12,
+    descrip: "descripcion Smartphone",
+    precioLista: 75000,
+    descuento: 10,
+    precioFinal: 67500 
+};
+
+let movil8 = {
+    id: 8,
+    nombre: "Apple XS" ,
+    marca: "apple",
+    os: "ios",
+    core: 8,
+    memory: 2,
+    MpxCamera: 12,
+    descrip: "descripcion Smartphone",
+    precioLista: 75000,
+    descuento: 10,
+    precioFinal: 67500 
+};
+
+let catalogo = [movil1, movil2, movil3, movil4, movil5, movil6, movil7,movil8]; 
+
+
+
 const productController = {
-    product:(req,res) => res.render("./products/product",{estilo:"product.css", titulo:"Celltech - Producto"}),
-    detail: (req,res) => res.render("./products/product",{estilo:"product.css", titulo:"Celltech - Producto"})
+    product:(req,res) => res.render("./products/product",{ moviles: [movil1, movil5] ,estilo:"product.css", titulo:"Celltech - Producto"}),
+    detail: (req,res) => res.render("./products/detail",{estilo:"product.css", titulo:"Celltech - Producto"})
 }
 
 module.exports= productController;
